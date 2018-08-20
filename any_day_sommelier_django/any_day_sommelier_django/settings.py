11807f7489084292e9cbd3334e4f82dcc72cfebe
Your Name <genejune4@hotmail.com>
2018-08-20 10:33:26 -0500

    things were added to make heroku work

--- any_day_sommelier_django/any_day_sommelier_django/settings.py
+++ any_day_sommelier_django/any_day_sommelier_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Any_Day_Sommelier'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'any_day_sommelier_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'any_day_sommelier',
+        'USER': 'sommelier',
+        'PASSWORD': 'anyday',
+        'HOST': 'localhost'
     }
 }
 
@@ -118,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
